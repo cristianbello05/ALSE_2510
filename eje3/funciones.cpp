0d8b0091862c7eb440b98a999160d7afb653dbd8
samueljim975 <samuel.jimenez-r@mail.escuelaing.edu.co>
2025-02-13 13:55:51 -0500

    Ejercicio clase 13 de Febrero

--- eje3/funciones.cpp
+++ eje3/funciones.cpp
@@ -14,6 +14,10 @@ double funeval( const double &x ){
   return sin(M_PI*5*x/100);
 }
 
+double funejercicio( const double &x){
+  return cos(x*M_PI/25);
+}
+
 void cruces_cero( double (*f)(const double &), const double &xl, const double &xu ){
   // El primer parámetro de esta función es un puntero a una función de la forma
   // double (*p)(const double&)
@@ -27,24 +31,12 @@ void cruces_cero( double (*f)(const double &), const double &xl, const double &x
 }
 
 int main(){
+  printf("Los cruces por cero de la funcion 1 son: \n");
   cruces_cero(funeval, 0, 100); // En este caso se llama la función Sin(M_PI*x/20)
+  printf("Los cruces por cero de la funcion 2 son: \n");
   cruces_cero(funeval2, 0, 100);// En este otro a sin(x/10)+cos(3*x/100)
+  printf("Los cruces por cero de la funcion del ejercicio son: \n");
+  cruces_cero(funejercicio, 0, 80);
   return 0;
 }
 
-
-/*
-int main(){
-  double anterior, actual, constante;
-  anterior = actual = 0.;
-  constante = 1.1 * M_PI * 5 / 100;
-
-  for(int i=0; i<100; i++){
-    actual = sin(constante * i);
-    if((anterior >= 0 && actual < 0 ) || (anterior < 0 && actual >= 0))
-      printf("Hay un cruce por cero en: %d\n",i);
-    anterior = actual;
-  }
-  return 0;
-}
-*/
