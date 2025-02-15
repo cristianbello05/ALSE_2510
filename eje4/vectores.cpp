74ddf5db675c3d6d5bb326e4a22ea21ac87f07e0
cristianbello05 <cristian.bello-v@mail.escuelaing.edu.co>
2025-02-15 16:19:47 -0500

    Modificacion producto punto

--- eje4/vectores.cpp
+++ eje4/vectores.cpp
@@ -9,30 +9,46 @@ using namespace std;
 double punto2( const double x[], const double y[], double r[], const int &cant ){
   // En esta función se hace el producto elemento a elemento de los dos vectores y se devuelve
   // la suma de todos los valores del vector de resultados.
+  double res = 0;
 
-  return 0;
+  for(int i=0; i<cant; i++){
+        r[i] = x[i] * y[i];
+        res = res + r[i];
+  }
+  return res;
 }
 
 double punto1( double *x, double *y, double *r, const int &cant ){
   // En esta función se hace el producto elemento a elemento de los dos vectores y se devuelve
   // la suma de todos los valores del vector de resultados.
-
-  return 0;
+double res = 0;
+  for(int i=0; i<cant; i++){
+        r[i] = x[i] * y[i];
+        res = res + r[i];
+  }
+  return res;
 }
-
 
 int main(){
   srand(time(0));
-  double v1[100]={3.}, v2[100], vres[100];
-
-  for(int i=0; i<100;i++)
+  double v1[15]/*={3.}*/, v2[15], vres[15];
+  for(int i=0; i<15;i++){
+    v1[i] = rand() % 10;
     v2[i] = 50 + rand() % 100;
+  }
 
-  cout << "La suma total con punto1 es: " <<  punto1(v1, v2, vres, 100);
-  cout << "La suma total con punto2 es: " <<  punto2(v1, v2, vres, 100);
+  cout << "La suma total con punto1 es: " <<  punto1(v1, v2, vres, 15);
+ printf("\n");
+  cout << "La suma total con punto2 es: " <<  punto2(v1, v2, vres, 15);
+ printf("\n");
 
-  for(int i=0; i<100;i++)
+  for(int i=0; i<15;i++)
     cout << "vres[" << i << "] = " << vres[i] << endl;
-
+ printf("\n");
+  for(int n=0; n<15;n++)
+    cout << "v1[" << n << "] = " << v1[n] << endl;
+ printf("\n");
+  for(int m=0; m<15;m++)
+    cout << "v2[" << m << "] = " << v2[m] << endl;
   return 0;
 }
